5885dea58abef73313d7c6a439ba898404e8b394
Kaipei0502 <113581835+Kaipei0502@users.noreply.github.com>
2023-02-16 23:55:05 +0800

    Add files via upload

---
+++ first.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>我的php</title>
+    </head>
+    <body>
+        <?php 
+            echo "Hello , World<br>";
+            $num = 1;
+            echo $num;
+        ?>
+    </body>
+</html>
